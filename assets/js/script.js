a4dd3f9ea9aa32ca0bcc70f62bae3231cea62f8a
MarynaPR <pryadkamaryna@gmail.com>
2020-09-26 11:20:11 -0500

    buttons for commands added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,103 @@
 //list all the var by ID
 var count = 0;
 var timerId;
+//global for later? when reducing by 10 sec
+var timeLeft = 60;
+//var time =questions.lenght*10
 
-//var startEl = document.getElementById("start");
+//all var buttons
 var startButton = document.getElementById("start");
+var clearButton = document.getElementById("clear");
+var submitButton = document.getElementById("submit");
+var rtrnButton = document.getElementById("return");
+//var for questions, choices, answers, timer, progress
+
 var question = document.getElementById("guestion");
 var timerEl = document.getElementById("timer");
 var choices = document.getElementById("choices");
-//to get to questions-display var for questions:
-var questionsEl = document.getElementById("questions");
 
-function (startQuiz) {
+var questionsEl = document.getElementById("questions");
+var progressEl = document.getElementById("progress");
+
+//start quiz function
+var startQuiz = function (event) {
+    console.log("test");
     //hide the first display page and move to question portion next by setAttribut and removeAttribute"class"-hide:
 
     var startPageEl = document.getElementById("start-page");
     startPageEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
 
-
     //start timer
-    timerId = setInterval(countDown, 1000);
+    //timerId = setInterval(countDown, 1000);
     //show starting time when question prompt:
-    timerEl.textContent = time - left;
-
+    //timerEl.textContent = time - left;
     getQuestion();
 };
 
-function (getQuestion) {
+var getQuestion = function (event) {
     //array of questions
+    var currentQuestion = questions[currentQuesitonIndex];
+    var displayEl = document.getElementById("question-display");
     //after the 1st question answered, either automatically or with 'next' button go to the next question
     // use loop for choices
+    //currentChoices
+    //for loop
+    //while loop
 
 
-    var questions = [
-        {
-            display: "Commonly used data types DO NOT include:",
-            choices: ["strings", "booleans", "alerts", "numbers"],
-            answer: "alerts"
-        },
-        {
-            display: "The condition in an if / else statement is enclosed within ____.",
-            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-            answer: "parentheses"
-        },
-        {
-            display: "Arrays in JavaScript can be used to store ____.",
-            choices: [
-                "numbers and strings",
-                "other arrays",
-                "booleans",
-                "all of the above"
-            ],
-            answer: "all of the above"
-        },
-        {
-            display:
-                "String values must be enclosed within ____ when being assigned to variables.",
-            choices: ["commas", "curly brackets", "quotes", "parentheses"],
-            answer: "quotes"
-        },
-        {
-            display:
-                "A very useful tool used during development and debugging for printing content to the debugger is:",
-            choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-            answer: "console.log"
-        }
-    ];
-};
-function (countDown) {
+    var questions = function (event) {
+        [
+            {
+                display: "Commonly used data types DO NOT include:",
+                choices: ["strings", "booleans", "alerts", "numbers"],
+                answer: "alerts"
+            },
+            {
+                display: "The condition in an if / else statement is enclosed within ____.",
+                choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+                answer: "parentheses"
+            },
+            {
+                display: "Arrays in JavaScript can be used to store ____.",
+                choices: [
+                    "numbers and strings",
+                    "other arrays",
+                    "booleans",
+                    "all of the above"
+                ],
+                answer: "all of the above"
+            },
+            {
+                display:
+                    "String values must be enclosed within ____ when being assigned to variables.",
+                choices: ["commas", "curly brackets", "quotes", "parentheses"],
+                answer: "quotes"
+            },
+            {
+                display:
+                    "A very useful tool used during development and debugging for printing content to the debugger is:",
+                choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+                answer: "console.log"
+            }
+        ];
+    };
+    var countdown = function (event) {
 
-};
+    };
 
-function (goBack) {
+    var return = function (event) {
+    };
+    var clearHighscores = function (event) { };
+    var viewHighScores = function (event) { };
+    var submit = function (event) { };
 
-};
-function (clearHighscores) {
+    //startButton.onclick = startQuiz;
+    startButton.addEventListener("click", startQuiz);
+    clearButton.addEventListener("click", clearHighscores);
+    submitButton.addEventListener("click", submit);
+    rtrnButton.addEventListener("click", return);
 
-};
-function (viewHighscores) {
-
-};
-//startButton.onclick = startQuiz;
-startButton.addEventListener("click", startQuiz);
 
 
 
