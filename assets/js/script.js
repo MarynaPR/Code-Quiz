df2bd065da00d06148bf25ea26b6bbc723073389
MarynaPR <pryadkamaryna@gmail.com>
2020-09-26 08:50:18 -0500

    first function startQuiz added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
 //list all the var by ID
-var startEl = document.getElementById("start");
+var count = 0;
+var timerId;
+
+//var startEl = document.getElementById("start");
+var startButton = document.getElementById("start");
 var question = document.getElementById("guestion");
 var timerEl = document.getElementById("timer");
 var choices = document.getElementById("choices");
+//to get to questions-display var for questions:
+var questionsEl = document.getElementById("questions");
 
-startEl.addEventListener("click", startQuiz);
 function (startQuiz) {
-    //hide the first display page
+    //hide the first display page and move to question portion next by setAttribut and removeAttribute"class"-hide:
+
+    var startPageEl = document.getElementById("start-page");
+    startPageEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+
+
     //start timer
-    //show questions with starting time
+    timerId = setInterval(countDown, 1000);
+    //show starting time when question prompt:
+    timerEl.textContent = time - left;
+
     getQuestion();
 };
 
@@ -17,42 +31,45 @@ function (getQuestion) {
     //after the 1st question answered, either automatically or with 'next' button go to the next question
     // use loop for choices
 
-};
-var questions = [
-    {
-        display: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        display: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        display: "Arrays in JavaScript can be used to store ____.",
-        choices: [
-            "numbers and strings",
-            "other arrays",
-            "booleans",
-            "all of the above"
-        ],
-        answer: "all of the above"
-    },
-    {
-        display:
-            "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "quotes"
-    },
-    {
-        display:
-            "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-        answer: "console.log"
-    }
-];
 
+    var questions = [
+        {
+            display: "Commonly used data types DO NOT include:",
+            choices: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts"
+        },
+        {
+            display: "The condition in an if / else statement is enclosed within ____.",
+            choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+            answer: "parentheses"
+        },
+        {
+            display: "Arrays in JavaScript can be used to store ____.",
+            choices: [
+                "numbers and strings",
+                "other arrays",
+                "booleans",
+                "all of the above"
+            ],
+            answer: "all of the above"
+        },
+        {
+            display:
+                "String values must be enclosed within ____ when being assigned to variables.",
+            choices: ["commas", "curly brackets", "quotes", "parentheses"],
+            answer: "quotes"
+        },
+        {
+            display:
+                "A very useful tool used during development and debugging for printing content to the debugger is:",
+            choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+            answer: "console.log"
+        }
+    ];
+};
+function (countDown) {
+
+};
 
 function (goBack) {
 
@@ -63,6 +80,9 @@ function (clearHighscores) {
 function (viewHighscores) {
 
 };
+//startButton.onclick = startQuiz;
+startButton.addEventListener("click", startQuiz);
+
 
 
 
